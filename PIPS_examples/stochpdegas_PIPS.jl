14c36e6a895642c2a8e1b6781f9aafd9ad9947d3
jalving <jalving@wisc.edu>
2016-05-25 15:59:39 -0500

    Updated PIPS notebooks JuMP syntax

--- PIPS_examples/stochpdegas_PIPS.jl
+++ PIPS_examples/stochpdegas_PIPS.jl
@@ -106,25 +106,25 @@ DEM =  keys(demDict)
 
 function createGasModel(SCEN)
 	 m = Model(solver=IpoptSolver())		 
-         @defVar(m, nodeDict[j].pmin<=p[SCEN, j in NODE, TIMEG]<=nodeDict[j].pmax, start= 50)             # node pressure - [bar]
-	 #@defVar(m, 0<=dp[SCEN, j = LINK,  TIMEG; linkDict[j].ltype == "a"]<=100, start= 10)             # compressor boost - [bar]
-	 @defVar(m, dp[SCEN, j = LINK,  TIMEG; linkDict[j].ltype == "a"], start= 10)                      # compressor boost - [bar]
-         @defVar(m, 1<=fin[SCEN, LINK, TIMEG]<=500, start= 100)                                           # flow in pipe - [scmx10-4/hr]
-         @defVar(m, 1<=fout[SCEN, LINK, TIMEG]<=500, start= 100)                                          # flow out pipe - [scmx10-4/hr]
-	 @defVar(m, 0.01<=sG[SCEN, j in SUP, TIMEG]<=supDict[j].max, start = 10)                          # supply flow - [scmx10-4/hr]
-	 @defVar(m, dem[SCEN, DEM, TIMEG],    start=100)                                                  # demand flow - [scmx10-4/hr]
-         @defVar(m, 0<=pow[SCEN, j = LINK, TIMEG; linkDict[j].ltype == "a"]<=3000, start= 1000)           # compressor power - [kW]
-	 @defVar(m, slack[SCEN, LINK, TIMEG, DIS]>=0, start= 10)                                          # auxiliary variable
+         @variable(m, nodeDict[j].pmin<=p[SCEN, j in NODE, TIMEG]<=nodeDict[j].pmax, start= 50)             # node pressure - [bar]
+	 #@variable(m, 0<=dp[SCEN, j = LINK,  TIMEG; linkDict[j].ltype == "a"]<=100, start= 10)             # compressor boost - [bar]
+	 @variable(m, dp[SCEN, j = LINK,  TIMEG; linkDict[j].ltype == "a"], start= 10)                      # compressor boost - [bar]
+         @variable(m, 1<=fin[SCEN, LINK, TIMEG]<=500, start= 100)                                           # flow in pipe - [scmx10-4/hr]
+         @variable(m, 1<=fout[SCEN, LINK, TIMEG]<=500, start= 100)                                          # flow out pipe - [scmx10-4/hr]
+	 @variable(m, 0.01<=sG[SCEN, j in SUP, TIMEG]<=supDict[j].max, start = 10)                          # supply flow - [scmx10-4/hr]
+	 @variable(m, dem[SCEN, DEM, TIMEG],    start=100)                                                  # demand flow - [scmx10-4/hr]
+         @variable(m, 0<=pow[SCEN, j = LINK, TIMEG; linkDict[j].ltype == "a"]<=3000, start= 1000)           # compressor power - [kW]
+	 @variable(m, slack[SCEN, LINK, TIMEG, DIS]>=0, start= 10)                                          # auxiliary variable
 
          # define spatio-temporal variables
-         @defVar(m, 10<=px[SCEN, LINK, TIMEG, DIS]<=100, start= 50)                                       # link pressure profile - [bar]
-         @defVar(m, 1<=fx[SCEN, LINK, TIMEG, DIS]<=100, start= 100)                                       # link flow profile - [scmx10-4/hr]
+         @variable(m, 10<=px[SCEN, LINK, TIMEG, DIS]<=100, start= 50)                                       # link pressure profile - [bar]
+         @variable(m, 1<=fx[SCEN, LINK, TIMEG, DIS]<=100, start= 100)                                       # link flow profile - [scmx10-4/hr]
         
 	 # compressor equations
-         @addNLConstraint(m, powereq[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "a"], pow[i,j,t] == c4*fin[i,j,t]*((px[i,j,t,1]/(px[i,j,t,1]-dp[i,j,t]))^om-1))
+         @NLconstraint(m, powereq[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "a"], pow[i,j,t] == c4*fin[i,j,t]*((px[i,j,t,1]/(px[i,j,t,1]-dp[i,j,t]))^om-1))
 
 	 # node balance [mass]
-         @addConstraint(m,nodemeq[k = SCEN, i in NODE, t = TIMEG], sum{       fout[k,j,t], j in LINK ; linkDict[j].endloc==i}
+         @constraint(m,nodemeq[k = SCEN, i in NODE, t = TIMEG], sum{       fout[k,j,t], j in LINK ; linkDict[j].endloc==i}
                                                                  - sum{       fin[k,j,t],  j in LINK ; linkDict[j].startloc==i}
                                                                  + sum{        sG[k,j,t],  j in SUP ; supDict[j].loc == i }
                                                                  - sum{       dem[k,j,t],  j in DEM ; demDict[j].loc == i }
@@ -132,46 +132,46 @@ function createGasModel(SCEN)
          
 
 	 # flow equations for passive and active links
-         @addConstraint(m, flow[i = SCEN, j = LINK, t = TIMEGm, k = 1:(Nx-1)], (px[i,j,t+1,k]-px[i,j,t,k])/dtG + linkDict[j].c1*(fx[i,j,t+1,k+1]-fx[i,j,t+1,k])/(linkDict[j].dx)==0)
+         @constraint(m, flow[i = SCEN, j = LINK, t = TIMEGm, k = 1:(Nx-1)], (px[i,j,t+1,k]-px[i,j,t,k])/dtG + linkDict[j].c1*(fx[i,j,t+1,k+1]-fx[i,j,t+1,k])/(linkDict[j].dx)==0)
 
          # boundary conditions flow
-         @addConstraint(m, flow_start[i = SCEN, j = LINK, t = TIMEG], fx[i,j,t,1]==fin[i,j,t])
-         @addConstraint(m, flow_end[i = SCEN, j = LINK, t = TIMEG], fx[i,j,t,Nx]==fout[i,j,t])
+         @constraint(m, flow_start[i = SCEN, j = LINK, t = TIMEG], fx[i,j,t,1]==fin[i,j,t])
+         @constraint(m, flow_end[i = SCEN, j = LINK, t = TIMEG], fx[i,j,t,Nx]==fout[i,j,t])
 
          # pressure equations for passive and active links
-         @addConstraint(m, press[i = SCEN, j = LINK, t = TIMEGm,k = 1:(Nx-1)], (fx[i,j,t+1,k]-fx[i,j,t,k])/dtG == - linkDict[j].c2*(px[i,j,t+1,k+1]-px[i,j,t+1,k])/linkDict[j].dx - slack[i,j,t+1,k])
-         @addNLConstraint(m, slackeq[i = SCEN, j = LINK, t = TIMEG, k = 1:Nx],  slack[i,j,t,k]*px[i,j,t,k] - linkDict[j].c3*fx[i,j,t,k]*fx[i,j,t,k] == 0);
+         @constraint(m, press[i = SCEN, j = LINK, t = TIMEGm,k = 1:(Nx-1)], (fx[i,j,t+1,k]-fx[i,j,t,k])/dtG == - linkDict[j].c2*(px[i,j,t+1,k+1]-px[i,j,t+1,k])/linkDict[j].dx - slack[i,j,t+1,k])
+         @NLconstraint(m, slackeq[i = SCEN, j = LINK, t = TIMEG, k = 1:Nx],  slack[i,j,t,k]*px[i,j,t,k] - linkDict[j].c3*fx[i,j,t,k]*fx[i,j,t,k] == 0);
 	
          # boundary conditions pressure, passive links
-         @addConstraint(m, presspas_start[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "p"], px[i,j,t,1] ==  p[i,linkDict[j].startloc,t])
-         @addConstraint(m,   presspas_end[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "p"], px[i,j,t,Nx] == p[i,linkDict[j].endloc,t])
+         @constraint(m, presspas_start[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "p"], px[i,j,t,1] ==  p[i,linkDict[j].startloc,t])
+         @constraint(m,   presspas_end[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "p"], px[i,j,t,Nx] == p[i,linkDict[j].endloc,t])
 
          # boundary conditions, active links
-         @addConstraint(m, pressact_start[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "a"], px[i,j,t,1] ==  p[i,linkDict[j].startloc,t] + dp[i,j,t])
-         @addConstraint(m,   pressact_end[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "a"], px[i,j,t,Nx] == p[i,linkDict[j].endloc,t])
+         @constraint(m, pressact_start[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "a"], px[i,j,t,1] ==  p[i,linkDict[j].startloc,t] + dp[i,j,t])
+         @constraint(m,   pressact_end[i = SCEN, j = LINK, t = TIMEG; linkDict[j].ltype == "a"], px[i,j,t,Nx] == p[i,linkDict[j].endloc,t])
 	 	 
 	 # fix pressure at supply nodes
-         @addConstraint(m, suppres[i = SCEN, j in SUP, t = TIMEG], p[i,supDict[j].loc,t] == nodeDict[supDict[j].loc].pmin)
+         @constraint(m, suppres[i = SCEN, j in SUP, t = TIMEG], p[i,supDict[j].loc,t] == nodeDict[supDict[j].loc].pmin)
 	 
 	 # discharge pressure for compressors
-	 @addConstraint(m, dispress[i in SCEN,j in LINK,t in TIMEG; linkDict[j].ltype=="a"],  p[i,linkDict[j].startloc,t]+dp[i,j,t]<=nodeDict[linkDict[j].startloc].pmax)
+	 @constraint(m, dispress[i in SCEN,j in LINK,t in TIMEG; linkDict[j].ltype=="a"],  p[i,linkDict[j].startloc,t]+dp[i,j,t]<=nodeDict[linkDict[j].startloc].pmax)
 
 	 # line pack constraints
-         @addConstraint(m, line_packT[i in SCEN],  sum{  sum{fx[i,j,Nt,k], k in DIS}*linkDict[j].dx, j in LINK} >= sum{ sum{fx[i,j,1,k],k in DIS}*linkDict[j].dx, j = LINK})
+         @constraint(m, line_packT[i in SCEN],  sum{  sum{fx[i,j,Nt,k], k in DIS}*linkDict[j].dx, j in LINK} >= sum{ sum{fx[i,j,1,k],k in DIS}*linkDict[j].dx, j = LINK})
 	 
          # ss constraints
-         @addConstraint(m, flow_ss[i = SCEN, j = LINK, t =0, k = 1:(Nx-1)], (fx[i,j,t+1,k+1]-fx[i,j,t+1,k])==0)
-         @addConstraint(m, pres_ss[i = SCEN, j = LINK, t =0, k = 1:(Nx-1)],  - linkDict[j].c2*(px[i,j, t+1,k+1]-px[i,j,t+1,k])/linkDict[j].dx - slack[i,j,t+1,k] == 0)
+         @constraint(m, flow_ss[i = SCEN, j = LINK, t =0, k = 1:(Nx-1)], (fx[i,j,t+1,k+1]-fx[i,j,t+1,k])==0)
+         @constraint(m, pres_ss[i = SCEN, j = LINK, t =0, k = 1:(Nx-1)],  - linkDict[j].c2*(px[i,j, t+1,k+1]-px[i,j,t+1,k])/linkDict[j].dx - slack[i,j,t+1,k] == 0)
 
-     	 @setObjective(m, Min, 1e-6*(1.0/S)*sum{ce*pow[i,j,t]*(dtG/3600),i= SCEN, j = LINK,t = TIMEG; linkDict[j].ltype == "a"}
+     	 @objective(m, Min, 1e-6*(1.0/S)*sum{ce*pow[i,j,t]*(dtG/3600),i= SCEN, j = LINK,t = TIMEG; linkDict[j].ltype == "a"}
 			      +1e-6*(1.0/S)*sum{cd*(dem[i,j,t]-demDict[j].stochd[i,t])^2, i in SCEN, j in DEM, t in TIMEG})
 			     
 	 
 	 # non-anticipativity constraints (note this is only for t==1)
 	 if (length(SCEN)> 1)
 	     bs = SCEN[1]
-	     @addConstraint(m,  nonantdq[i in SCEN,j in LINK,t in TIMEG; linkDict[j].ltype =="a" && t ==1 && i != bs],   dp[i,j,t] ==  dp[bs,j,t])
-	     @addConstraint(m,  nonantde[i in SCEN,j in DEM, t in TIMEG;                            t ==1 && i != bs],   dem[i,j,t]== dem[bs,j,t])
+	     @constraint(m,  nonantdq[i in SCEN,j in LINK,t in TIMEG; linkDict[j].ltype =="a" && t ==1 && i != bs],   dp[i,j,t] ==  dp[bs,j,t])
+	     @constraint(m,  nonantde[i in SCEN,j in DEM, t in TIMEG;                            t ==1 && i != bs],   dem[i,j,t]== dem[bs,j,t])
 	 end
 	 
 	 return m
@@ -185,13 +185,13 @@ println("obj ", getObjectiveValue(IL))
 
 
 IL = NetModel()
-@defVar(IL, dp[j = LINK; linkDict[j].ltype == "a"], start= 10)                       # compressor boost - [bar]
-@defVar(IL, dem[DEM],    start=100)                                                  # demand flow - [scmx10-4/hr]
+@variable(IL, dp[j = LINK; linkDict[j].ltype == "a"], start= 10)                       # compressor boost - [bar]
+@variable(IL, dem[DEM],    start=100)                                                  # demand flow - [scmx10-4/hr]
 for s in SCENG
    single_scenario = createGasModel(s:s) 
    @addNode(IL, single_scenario, "s$s")
-   @addConstraint(IL,  nonantdq[j in LINK,t in TIMEG; linkDict[j].ltype =="a" && t ==1],   dp[j] ==  getVar(single_scenario, :dp)[s,j,t])
-   @addConstraint(IL,  nonantde[j in DEM, t in TIMEG;                            t ==1],   dem[j]==  getVar(single_scenario,:dem)[s,j,t])
+   @constraint(IL,  nonantdq[j in LINK,t in TIMEG; linkDict[j].ltype =="a" && t ==1],   dp[j] ==  getvariable(single_scenario, :dp)[s,j,t])
+   @constraint(IL,  nonantde[j in DEM, t in TIMEG;                            t ==1],   dem[j]==  getvariable(single_scenario,:dem)[s,j,t])
 end
 ParPipsNlp_solve(IL)
 
