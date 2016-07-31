c91a9a99680c9415811c2615956999b263f8464b
Victor M Zavala <victor.zavala@wisc.edu>
2016-07-31 06:54:42 -0500

    adding julia code

---
+++ UWMadison2016/stochprog/ex1gaussfullpyplot.jl
@@ -0,0 +1,70 @@
+
+# Loading packages:
+using JuMP 
+using Distributions 
+using Ipopt
+using PyPlot
+
+# Generate random data: 
+NS = 100
+S = collect(1:NS); 
+srand(0)
+μ = 0; σ = 2; 
+d = Normal(μ,σ)
+R = rand(d,NS);
+
+# Plotting data
+plt[:hist](R, bins = 30);
+grid("on")
+xlabel(L"\xi")
+ylabel(L"p(\xi)")
+
+# ex1gauss.mod 
+m = Model(solver=IpoptSolver(print_level=0))
+
+@variable(m, x)            # decision variable
+@variable(m, cost[1:NS])   # per scenario cost
+@constraint(m, costeq[s in S], cost[s] == (x-R[s])^2) 
+
+@objective(m, Min, (1/NS)*sum{cost[s], s in S})
+
+solve(m)
+solcost = getvalue(cost);
+println("x=",getvalue(x))
+println("mean=",getobjectivevalue(m))
+println("meanR=",(1/NS)*sum(R))
+
+# Plotting cost fistribution 
+plt[:hist](solcost,bins = 30)
+grid("on")
+xlabel(L"f(x^*(\xi),\xi)")
+ylabel(L"p(f(\cdot))")
+
+m = Model(solver=IpoptSolver(print_level=0))
+
+@variable(m, x)              # decision variable
+@variable(m, cost[1:NS])     # per scenario cost
+@variable(m, VaR)            # cvar auxiliary variable
+@variable(m, phi[S] >= 0)    # cvar auxiliary variable
+
+# set cvar probability level
+alpha = 0.0001;               
+
+@constraint(m, costeq[s in S], cost[s] == (x-R[s])^2) 
+@constraint(m, cvar[s in S], cost[s]-VaR <= phi[s])
+
+@objective(m, Min, VaR + (1/NS)*sum{(1/alpha)*phi[s], s in S})
+
+solve(m)
+solcost2 = getvalue(cost);
+println("x=",getvalue(x))
+println("CVaR=",getobjectivevalue(m))
+println("mean=",(1/NS)*sum(solcost2))
+
+# Plotting cost fistribution 
+plt[:hist](solcost2,bins = 30)
+grid("on")
+xlabel(L"f(x^*(\xi),\xi)")
+ylabel(L"p(f(\cdot))")
+
+solcost
