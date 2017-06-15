ca6596925b46d327e783fc153f8c4f88ce40d86a
Victor Zavala Tejeda <zavalatejeda@vzt07.che.wisc.edu>
2017-06-15 17:03:19 -0500

    adding new stochnlp examples

---
+++ Valladolid2017/StochNLP/Plasmo.jl/src/Plasmo.jl
@@ -0,0 +1,114 @@
+# Plasmo Code
+# Yankai Cao, Victor Zavala
+# UW-Madison, 2016
+
+module Plasmo
+import JuMP
+export NetModel, @addNode, getNode, @Linkingconstraint
+export getparent, getchildren 
+export Ipopt_solve
+export PipsNlp_solve
+using Base.Meta
+
+type NetData
+    children::Vector{JuMP.Model}
+    parent
+    childrenDict::Dict{ASCIIString, JuMP.Model}
+end
+NetData() = NetData(JuMP.Model[], nothing, Dict{ASCIIString, JuMP.Model}())
+
+
+function NetModel(buildType="serial")
+    m = JuMP.Model()
+    m.ext[:Net] = NetData()
+    m.ext[:BuildType] = buildType
+    m.ext[:linkingId] = []
+    return m
+end
+
+function getNet(m::JuMP.Model)
+    if haskey(m.ext, :Net)
+        return m.ext[:Net]
+    else
+        error("This functionality is only available")
+    end
+end
+
+getparent(m::JuMP.Model)       = getNet(m).parent
+#getchildren(m::JuMP.Model)     = getNet(m).children
+getchildrenDict(m::JuMP.Model)     = getNet(m).childrenDict
+
+getname(c::Symbol) = c
+getname(c::Void) = ()
+getname(c::Expr) = c.args[1]
+
+function getchildren(m::JuMP.Model)
+    if haskey(m.ext, :Net)
+        return getNet(m).children
+    else
+        return []
+    end
+end
+
+function getNode(m::JuMP.Model, modelname::ASCIIString)
+    if !haskey(getNet(m).childrenDict, modelname)
+        error("No model with name $modelname")
+    elseif getNet(m).childrenDict[modelname] === nothing
+        error("Multiple models with name $modelname")
+    else
+        return getNet(m).childrenDict[modelname]
+    end
+end
+
+
+function registermodel(m::JuMP.Model, modelname::ASCIIString, value::JuMP.Model)
+    if haskey(getNet(m).childrenDict, modelname)
+        getNet(m).childrenDict[modelname] = nothing # indicate duplicate
+        error("Multiple models with name $modelname")
+    else
+        getNet(m).childrenDict[modelname] = value
+    end
+end
+
+macro Linkingconstraint(m, args...)
+      expr = quote
+      	  id_start = length($(m).linconstr) + 1
+          @constraint($(m),$(args...))
+	  id_end = length($(m).linconstr)
+	  $(m).ext[:linkingId] = [$(m).ext[:linkingId]; id_start:id_end]   
+      end
+      return esc(expr)
+end
+
+macro addNode(m, node)
+    if isa(node, Symbol)
+       return quote       	    
+    	   if haskey($(esc(node)).ext, :Net)
+	        getNet($(esc(node))).parent = $(esc(m))
+    	   else
+		$(esc(node)).ext[:Net] = NetData(JuMP.Model[], $(esc(m)),Dict{Symbol, JuMP.Model}())
+           end
+	   push!(getNet($(esc(m))).children, $(esc(node)))
+    	   registermodel($(esc(m)), string($(quot(node))), $(esc(node)))
+       end
+    else
+	error("not supported")
+    end
+end
+
+macro addNode(m, node, nodename)
+       return quote
+           if haskey($(esc(node)).ext, :Net)
+                getNet($(esc(node))).parent = $(esc(m))
+           else
+                $(esc(node)).ext[:Net] = NetData(JuMP.Model[], $(esc(m)),Dict{Symbol, JuMP.Model}())
+           end
+           push!(getNet($(esc(m))).children, $(esc(node)))
+           registermodel($(esc(m)), $(esc(nodename)), $(esc(node)))
+       end
+end
+
+end
+include("NetParPipsNlp.jl")
+include("NetIpopt.jl")
+
---
+++ Valladolid2017/StochNLP/stochpdegas_plasmo.jl
@@ -0,0 +1,128 @@
+# stochastic optimal control problem PLASMO and PIPS-NLP/IPOPT
+# Yankai Cao and Victor M. Zavala
+# University of Wisconsin-Madison, 2016
+
+push!(LOAD_PATH, pwd())
+using Ipopt
+using Plasmo
+using JuMP
+
+MPI.Init()  # Initialize MPI
+
+# sets
+TF=24*3600                           # horizon time - [s]
+Nt=24                                # number of temporal grid points
+Nx=3                                 # number of spatial grid points
+S=3                                  # number of scenarios
+TIMEG=1:Nt                           # set of temporal grid points
+TIMEGm=1:Nt-1                        # set of temporal grid points minus 1
+DIS=1:Nx                             # set of spatial grid points
+SCENG=1:S                            # scenario set
+
+# links
+type LinkData                        # set of links
+     name::ASCIIString
+     startloc::ASCIIString           # start node
+     endloc::ASCIIString             # end node
+     diam::Float64                   # link diameter - mm
+     length::Float64                 # link length - km
+     ltype::ASCIIString              # link type, passive or active
+     c1                              # aux constant 
+     c2                              # aux constant 
+     c3                              # aux constant 
+     dx                              # spatial grid spacing - [m]     
+     lam                             # friction coefficient - []
+     A                               # pipe transveral area - [m^2]
+end
+linkDict = Dict{ASCIIString, LinkData}()
+
+# nodes
+type NodeData
+     name::ASCIIString
+     pmin::Float64		          # min pessure - bar
+     pmax::Float64  		      # max pressure - bar
+end
+nodeDict = Dict{ASCIIString, NodeData}()
+
+# supply
+type SupplyData                   # set of suppliers
+     name::ASCIIString
+     loc::ASCIIString		      # supply location
+     min::Float64   		      # min supply - scmx106/day
+     max::Float64   		      # max supply - scmx106/day
+end
+supDict = Dict{ASCIIString, SupplyData}()
+
+# demand
+type DemandData                    # set of suppliers
+     name::ASCIIString
+     loc::ASCIIString    	       # demand location
+     d::Float64                    # base demand - scmx106/day
+     stochd			               # stochastic demands - [scmx10-4/hr]
+end
+demDict = Dict{ASCIIString, DemandData}()
+
+# physical data
+eps= 0.025            # pipe rugosity - [mm]
+z= 0.80               # gas compressibility  - []
+rhon=0.72             # density of air at normal conditions - [kg/m3]
+R=8314.0              # universal gas constant [J/kgmol-K]
+M=18.0                # gas molar mass [kg/kgmol]
+pi=3.14               # pi
+nu2=0                 # gas speed of sound [m2/s2]
+Tgas = 293.15         # reference temperature [K]
+Cp = 2.34             # heat capacity @ constant pressure [kJ/kg-K]
+Cv = 1.85             # heat capacity @ constant volume [kJ/kg-K]
+gam = Cp/Cv           # expansion coefficient [-]
+om = (gam-1.0)/gam    # aux constant [-]
+U = 1.0*0.1           # pipe heat transfer coefficient [J/m2-s-K]
+Tamb = 20+273.15      # soil temperature [K]
+Tsup = 30+273.15      # supply temperature [K]
+
+# scaling and constants
+ffac = 0              # from scmx106/day to kg/s
+ffac2 = 0             # from kg/s to scmx10-4/hr
+pfac = 0              # from bar to Pa
+pfac2 = 0             # from Pa to bar
+dfac = 0              # from mm to m
+lfac = 0              # from km to m
+c4 = 0                # aux constant [kW/(scmx10-4/hr)]
+
+# cost factors
+ce = 0.1             # cost of compression [$/kWh]
+cd = 1e6             # demand tracking cost [-]
+cT = 1e6             # terminal constraint cost [-]
+cs =   0             # supply cost [$/scmx10-4]
+
+# define temporal discretization info
+dtG = 0              # temporal grid spacing - [s]
+
+include("set_data_paper.jl")
+NODE = keys(nodeDict)
+LINK = keys(linkDict)
+SUP =  keys(supDict)
+DEM =  keys(demDict)
+
+# define scenario model
+include("createGasModel.jl")
+
+# define PLASMO model and parent variables
+IL = NetModel()
+@variable(IL, dp[j = LINK; linkDict[j].ltype == "a"], start= 10)                       # compressor boost - [bar]
+@variable(IL, dem[DEM],    start=100)                                                  # demand flow - [scmx10-4/hr]
+
+# define children models
+for s in SCENG
+   gasch = createGasModel(s) 
+   @addNode(IL, gasch, "children$s")
+   @constraint(IL,  nonantdq[j in LINK,t in TIMEG; linkDict[j].ltype =="a" && t ==1],   dp[j] ==  getvariable(gasch, :dp)[j,t])
+   @constraint(IL,  nonantde[j in DEM, t in TIMEG;                            t ==1],   dem[j]==  getvariable(gasch,:dem)[j,t])
+end
+
+# solve with IPOPT
+Ipopt_solve(IL)
+
+# solve with PIPS-NLP
+#ParPipsNlp_solve(IL)
+
+MPI.Finalize()
