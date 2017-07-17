cff6324fcd27ba847c68d543805bd95b110c3814
Victor Zavala <vzavala@Krylov-4.local>
2017-07-17 06:44:38 -0500

    no message

--- Valladolid2017/Formulations/stochmoo.jl
+++ Valladolid2017/Formulations/stochmoo.jl
@@ -12,14 +12,15 @@ nObj = 2
 # number of stakeholders
 nStake = 20;
 
+# get weights and normalize them
 W = rand(nStake,nObj)
 for i = 1:size(W,1)
 W[i,:] = W[i,:] / sum(W[i,:])
 end
 
 # container for solutions
-x1v=zeros(nStake)
-x2v=zeros(nStake)
+f1v=zeros(nStake)
+f2v=zeros(nStake)
 cp=zeros(nStake)
 
 for p in 1:nStake
@@ -38,8 +39,8 @@ m = Model(solver=IpoptSolver(print_level=0))
 solve(m)
     
 # collect solution    
-    x1v[p]=getvalue(x1);
-    x2v[p]=getvalue(x2);
+    f1v[p]=(getvalue(x1)-1)^2
+    f2v[p]=(getvalue(x2)-getvalue(x1))^2
     cp[p]=getvalue(cost)
 end
 
@@ -52,11 +53,11 @@ m = Model(solver=IpoptSolver(tol = 1e-4, max_iter = 100,linear_solver ="mumps",m
 @variable(m, 0 <= x1<= 1)    
 @variable(m, 0 <= x2<= 1)  
 @variable(m, phi[P]>=0)
-@variable(m, cost[P])
+@variable(m, dis[P])
 @variable(m, VaR)
 @NLconstraint(m, cons, x1^2 + x2 == 1 ) 
-@NLconstraint(m,costcons[p in P],cost[p]== W[p,1]*(x1-1)^2 + W[p,2]*(x2-x1)^2)
-@NLconstraint(m,cvarcons[p in P],cost[p] - VaR <= phi[p])
+@NLconstraint(m,costcons[p in P],dis[p]== W[p,1]*(x1-1)^2 + W[p,2]*(x2-x1)^2-cp[p])
+@NLconstraint(m,cvarcons[p in P],dis[p] - VaR <= phi[p])
 @NLobjective(m, Min, VaR+(1/(alpha*nStake))*sum(phi[p] for p in P))
    
 # solve model and get solution
@@ -65,15 +66,12 @@ solve(m)
 
 cx1=zeros(1)
 cx2=zeros(1)
-cx1=getvalue(x1)
-cx2=getvalue(x2)
+cx1=(getvalue(x1)-1)^2
+cx2=(getvalue(x2)-getvalue(x1))^2
 
-# visualize Pareto set
-scatter(x1v,x2v,color="blue",marker="o")
+# visualize Pareto set and compromise
+scatter(f1v,f2v,color="blue",marker="o")
 grid("on")
-hold("on")
 scatter(cx1,cx2,color="red",marker=".")
 xlabel("f1")
 ylabel("f2")
-
-
