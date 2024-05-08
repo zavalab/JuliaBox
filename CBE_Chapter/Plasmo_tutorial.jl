154a3955c37c17b49a3d093ad34062dddcfb5bfa
dlcole3 <dlcole3@wisc.edu>
2024-05-08 11:33:03 -0500

    Added comments to Plasmo tutorial

--- CBE_Chapter/Plasmo_tutorial.jl
+++ CBE_Chapter/Plasmo_tutorial.jl
@@ -1,28 +1,35 @@
 using Plasmo, Ipopt
 
+# Define OptiGraph
 graph = OptiGraph()
 
+# Define OptiNodes
 @optinode(graph, n1)
 @optinode(graph, n2)
 @optinode(graph, n3)
 
+# Add variables, constraint, and objective to OptiNode 1
 @variable(n1, x >= 0)
 @variable(n1, y >= 2)
 @constraint(n1, x + y >= 3)
 @objective(n1, Min, 2 * x + y)
 
+# Add variables, constraint, and objective to OptiNode 2
 @variable(n2, x >= 0)
 @variable(n2, y >= 1)
 @constraint(n2, x + 2 * y >= 4)
 @objective(n2, Min, 2 * x + y)
 
+# Add variables, constraint, and objective to OptiNode 3
 @variable(n3, x >= 1)
 @variable(n3, y >= 0)
 @constraint(n3, x + 3 * y >= 2)
 @objective(n3, Min, 2 * x + y)
 
+# Define linking constraints
 @linkconstraint(graph, n1[:x] + n2[:x] == 1)
 @linkconstraint(graph, n1[:x] + n2[:x] + n3[:x] == 4)
 
+# Optimize
 set_optimizer(graph, Ipopt.Optimizer)
 optimize!(graph)
